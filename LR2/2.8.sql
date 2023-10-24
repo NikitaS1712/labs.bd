af3e5f27e1729e9321786ab1ea52f7371a06f7f0
Nikita Solodovnikov <data.sv03@gmail.com>
2023-10-24 13:00:30 +0500

    lr2-2.8

---
+++ LR2/2.8.sql
@@ -0,0 +1,7 @@
+USE cd;
+/* Выбрать имена объектов с их качественной стоимостью
+ (качественной стоимостью == ‘дешевый’ если monthlymaintenance < 100 и 
+ ‘дорогой’ в противном случае*/
+SELECT facility, 'Дешевый' AS 'Качественная стоимость' FROM facilities WHERE monthlymaintenance < 100
+UNION
+SELECT facility, 'Дорогой' FROM facilities WHERE monthlymaintenance >= 100;
