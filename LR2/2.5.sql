1c86afbbbbe64a9706aa8fbbe94f3fb32ea65206
Nikita Solodovnikov <data.sv03@gmail.com>
2023-10-24 12:57:44 +0500

    lr2-2.5

---
+++ LR2/2.5.sql
@@ -0,0 +1,3 @@
+USE cd;
+/*Выбрать объекты, которые платны для членов клуба, но стоимость использование не превышает 1/50 от стоимость месячного обслуживания */
+SELECT facility  FROM facilities  WHERE membercost != 0 AND membercost/monthlymaintenance <= 1/50;
