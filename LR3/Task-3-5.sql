487f3652f7922af2e8d9065c45f72c0611d1ccc9
Nikita Solodovnikov <data.sv03@gmail.com>
2023-11-13 10:13:12 +0500

    lr3-5

---
+++ LR3/Task-3-5.sql
@@ -0,0 +1,6 @@
+USE cd;
+/* Выберите ФИО (== имя + фамилия) всех, кто покупал корты 1 и 2. */
+SELECT DISTINCT surname, firstname FROM members
+JOIN bookings ON members.memid = bookings.memid
+JOIN facilities ON facilities.facid = bookings.facid
+WHERE facilities.facility LIKE '%Tennis court%';
