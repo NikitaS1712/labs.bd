a78d2139785b92a6f90a07227a3ea5a113acd9fb
Nikita Solodovnikov <data.sv03@gmail.com>
2023-11-13 13:00:53 +0500

    lr4-4

---
+++ LR4/Task-4-4.sql
@@ -0,0 +1,6 @@
+USE cd;
+/* Реальные затраты на создание корта 2 (facid =1) составили 10000. Обновите данные в таблице, чтобы учесть это изменение. */
+UPDATE facilities
+SET initialoutlay = 10000 
+WHERE facid =1;
+SELECT * FROM facilities;
