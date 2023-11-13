f52441c2eeb4da444bb1d4c80a206ce1f00769fc
Nikita Solodovnikov <data.sv03@gmail.com>
2023-11-13 10:05:32 +0500

    lr3-4

---
+++ LR3/Task-3-4.sql
@@ -0,0 +1,6 @@
+USE cd;
+/* Выберите всех членов клуба и членов, которые их рекомендовали,  отсортировав их по имени и фамилии. */
+SELECT m1.surname, m1.firstname, m2.surname AS "Recommender surname", m2.firstname AS "Recommender firstname"
+FROM members m1
+LEFT JOIN members m2 ON m1.recommendedby = m2.memid
+ORDER BY m1.firstname;  
