1482c26932043c8c8ee7566c4959f3c66e1c94a8
Nikita Solodovnikov <data.sv03@gmail.com>
2023-11-13 12:55:03 +0500

    lr4-3

---
+++ LR4/Task-4-3.sql
@@ -0,0 +1,5 @@
+USE cd;
+/*Добавьте еще одно spa с такими же характеристиками как в задании 1, сгенерировав новое ID.*/
+INSERT INTO facilities (facid, facility, membercost, guestcost, initialoutlay,monthlymaintenance )
+SELECT 12, facility, membercost, guestcost, initialoutlay,monthlymaintenance  FROM facilities WHERE facid = 9;
+SELECT * FROM facilities;
