b34602b984c87c49ab4dc2bcb9f83fd7e70b88b0
Nikita Solodovnikov <data.sv03@gmail.com>
2023-12-01 15:58:10 +0500

    lr6-5

--- LR6/Task-6-5.sql
+++ LR6/Task-6-5.sql
@@ -1,4 +1,5 @@
 USE cd;
 /*Выведите наименования всех объектов клуба заглавными буквами, если они содержат в названии слово ‘Tennis’*/
-SELECT facility FROM facilities 
+SELECT UPPER(facility) as facility
+FROM cd.facilities 
 WHERE BINARY facility LIKE '%Tennis%';
