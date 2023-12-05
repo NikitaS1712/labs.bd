aed2739d7add51aaaf3544528f99adaf633a626f
Nikita Solodovnikov <data.sv03@gmail.com>
2023-12-05 13:17:16 +0500

    lr7-1

---
+++ LR7/Task-7-1.sql
@@ -0,0 +1,17 @@
+USE cd;
+/*Создайте функцию, которая рассчитывает стоимость каждой аренды 
+(для каждой записи таблицы bookings).*/
+DELIMITER $$
+DROP FUNCTION IF EXISTS CalculateRentalCost;
+CREATE FUNCTION CalculateRentalCost(memid INT, facid INT, slots INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+DECLARE Стоимость_аренды INT;
+SET Стоимость_аренды = (SELECT IF(memid = 0, guestcost * slots, membercost * slots)
+FROM facilities f WHERE facid = f.facid);
+RETURN Стоимость_аренды;
+END$$
+DELIMITER;
+SELECT CalculateRentalCost(memid, facid, slots) AS Стоимость_аренды
+FROM bookings;
