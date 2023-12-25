7faca9b7d67ad55d9348f7c57875112498078f3d
Nikita Solodovnikov <data.sv03@gmail.com>
2023-12-25 12:15:13 +0500

    lr8

--- LR8/Task1.sql
+++ LR8/Task1.sql
@@ -1,10 +1,10 @@
 /* Самый кассовый фильм месяца */
 SELECT f.titleFIL AS 'Название фильма', 
-       SUM(a.number_of_tickets * p.cost) AS 'Выручка' 
+       MONTH(p.startDate) AS 'Месяц', SUM(a.number_of_tickets * p.cost) AS 'Выручка'
 FROM Films f 
 INNER JOIN Poster p ON f.idFIL = p.film 
 INNER JOIN Audience a ON p.idPOS = a.poster_number 
-GROUP BY f.titleFIL 
+GROUP BY f.titleFIL, p.startDate
 ORDER BY SUM(a.number_of_tickets * p.cost) DESC 
 LIMIT 1;
 
@@ -13,19 +13,18 @@ SELECT C.titleCIN, C.address, C.number_of_places - SUM(A.number_of_tickets) AS f
 FROM Cinemas C
 JOIN Poster P ON C.idCIN = P.cinema
 LEFT JOIN Audience A ON P.idPOS = A.poster_number
-WHERE P.startDate <= '2022-01-19' AND P.endDate >= '2022-01-01'
 GROUP BY C.idCIN, A.visiting_day
 HAVING freeseats > 0
 ORDER BY freeseats DESC;
 
 /* Фильм с указанием максимального количества зрителей. */
-SELECT F.titleFIL, MAX(A.total_tickets) AS max_daily_audience
+SELECT F.titleFIL, MAX(A.total_tickets) AS max_daily_audience, DAY(a.visiting_day)
 FROM Films F
 JOIN Poster P ON F.idFIL = P.film
 JOIN (
-    SELECT poster_number, SUM(number_of_tickets) AS total_tickets
+    SELECT poster_number, visiting_day, SUM(number_of_tickets) AS total_tickets
     FROM Audience
-    GROUP BY poster_number
+    GROUP BY poster_number, visiting_day
 ) A ON P.idPOS = A.poster_number
-GROUP BY F.idFIL
+GROUP BY F.titleFIL, a.visiting_day
 ORDER BY max_daily_audience DESC;
